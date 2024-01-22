b152b96ae926187d24201b04c4851164f0574e16
javeria-farooq <gettingeducation6@gmail.com>
2024-01-22 13:08:32 +0500

    19 JS Assignment

---
+++ app.js
@@ -0,0 +1,34 @@
+// function myFunction() {
+//     alert('button click hgya')
+// }
+
+// hm ye chah rh hn k jb button k upr click hto para show hjae ab mn function mn jaoungi or mujhe para khn show krwna h
+// document pe show krwna h t mn function mn jaake lkhngi document. or wese bh hmn pta h k jb bh hm document lkhte hn t
+// hmari ye file tmam html k elements ko get krlgi ab lkn agr html mn itni saari cheezen pri v hgi t ye t sb ko hi get
+// krlga lkn mujhe t ye function srf paragaraph pe chlana h sb pe nh chlana. t ab is k lye mn para ko kse get krngi
+// zhir h uska tag t lkhngi nh islye uski id ko get krngi ab id ko get krne ka treeqa kia h. mne jb document lkha t
+// usne sbko get krlia phr mne btaya k nh mujhe ek element jo k id k through get krna h. t mn lkhngi k get element
+// by id: document.getElementById. ab zhir h iske undr mujheb btana bh hg ak knsi id ko get krna h t mne round braces
+// lgae qk ye bh ek function h or round braces k under string bna k us id ka name lkh dia. ab mn khungi k tmne id ko
+// get t krlia h lkn ab oose get krke wo kaam bh t krwao jo mn tmhn bta rh hn ab wo kaam mn oose kse khungi. wo kaam
+// mn oose khungi .innerHTML mtlb uska inner html jo th oose is function mn change krdo phr = lga k khungi k or brbr
+// krdo is cheez k yni = dlne k baad hm wo lkhnge jske brbr hmn krna h yni jo hmn print krwna h ab chahe wo string mn
+// ho ya int mn.
+
+// function myFunction() {
+//     document.getElementById('anc').style.display = none;
+//     document.getElementById('showPara').innerHTML = 'Working like a whatsapp has done!'
+// }
+
+// ab ye read more gyb hoke para t show hgya lkn ye qk anchor k tag mn aaaya h is wjah se ye para blue link bn k aarh
+// h or hm chahte hn k ye simple para ki trhn aaye or read more bh gyb hjae t mn kia krngi mn us para ko read more ki
+// link mn nh blke para ki id mn rkhngi but onclick event k or read more ki link ko id dke app.js mn usko getElementById
+// dke style.display = none; krdngi jsse read more ki link click krne pe gyb hjaegi.
+// ab ye hello world jo h wo khn jaega ye jaega us id mn jo hmne oose di h ab wo id pe jaake ye uska inner html change
+// krdga or ye change kb krega jb ye function cl hga or function cl kb hrh h jb hm button ko click krrh hn.
+
+
+// function myFunction() {
+//     document.getElementById('sp').innerHTML = 'DONE'
+//     document.getElementById('anc').style.display = 'none';
+// }
